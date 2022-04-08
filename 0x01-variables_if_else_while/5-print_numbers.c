07a7bb1e5df3d9402be95729b92e7737b2363b06
Sylvester254 <slynganga59@gmail.com>
2022-04-08 17:54:08 +0300

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+/**
+ * main - Prints the numbers from 0-9.
+(*
+ * Return: 0 on success
+ */
 int main(void)
 {
 int x = 0;
-while (x <= 10)
+while (x < 10)
 {
-printf("%d\n", x);
+printf("%d ", x);
 x++;
 }
-return(0);    
+printf("\n");
+    return(0);
 }
