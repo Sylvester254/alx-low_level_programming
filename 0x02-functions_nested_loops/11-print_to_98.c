2e7566066b64dd7a1a24b8c2d883a9786bafe62d
Sylvester254 <slynganga59@gmail.com>
2022-04-13 23:58:19 +0300

    print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,47 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_times_table - prints the times table for n.
- * @n: The multiplication table requested.
- * Return: Nothing.
+ * print_to_98 - prints all 
+ * natural numbers from n to 98
+ * Return: void
  */
-void print_times_table(int n)
+void print_to_98(int n)
 {
-int i, j, res;
-
-	if (!(n > 15 || n < 0))
+	if (n < 99)
 	{
-		for (i = 0; i <= n; i++)
+		for (n = n; n < 98; n++)
 		{
-			for (j = 0; j <= n; j++)
-			{
-				res = (i * j);
-				if (j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				if (res < 10 && j != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 10 && res < 100)
-				{
-					_putchar(' ');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 100 && j != 0)
-				{
-					_putchar((res / 100) + '0');
-					_putchar((res / 10) % 10 + '0');
-					_putchar((res % 10) + '0');
-				}
-				else
-					_putchar((res % 10) + '0');
-			}
-			_putchar('\n');
+			printf("%d, ", n);
+		}
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+		{
+			printf("%d, ", n);
 		}
 	}
 }
