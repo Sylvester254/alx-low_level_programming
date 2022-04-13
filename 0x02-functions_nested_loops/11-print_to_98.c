b05f4e54efaeee53ea789de089ccb96c6c08f97b
Silver <79365412+Sylvester254@users.noreply.github.com>
2022-04-14 00:15:44 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - prints all 
- * natural numbers from n to 98
- * Return: void
+ *print_to_98 - prints all
+ *natural numbers from n to 98
+ *Return: void
  */
 void print_to_98(int n)
 {
