fb6ec819420796c5d03a2556331f2eb817fad0e1
Sylvester254 <slynganga59@gmail.com>
2022-04-14 23:42:31 +0300

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies a and b
+ * @a: 1st value
+ * @b: 2nd value
+ */
+int mul(int a, int b)
+{
+int res = a * b;
+return (res);
+}
--- 0x04-more_functions_nested_loops/f-test.c
+++ 0x04-more_functions_nested_loops/f-test.c
@@ -8,26 +8,17 @@
  */
 int main(void)
 {
-    char c;
-
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
+    printf("%d\n", mul(98, 1024));
+    printf("%d\n", mul(-402, 4096));
     return (0);
 }
 /**
- * _isdigit - function checks for a digit 
- * @c: character to be checked.
- * Return: 1 if the character is a digit.
- * Return: 0 if the character is a digit.
+ * mul - multiplies a and b
+ * @a: 1st value
+ * @b: 2nd value
  */
-int _isdigit(int c)
+int mul(int a, int b)
 {
-if (c >= '0' && c <= '9')
-{
-return(1);
-}
-else
-return(0); 
+int res = a * b;
+return(res);
 }
