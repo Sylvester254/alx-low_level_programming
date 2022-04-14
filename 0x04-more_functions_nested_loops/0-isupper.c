ec9ebb35bbfd1f87f99bde868725dde604960e22
Sylvester254 <slynganga59@gmail.com>
2022-04-14 22:49:03 +0300

    isupper or lower

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - tests whether a character in the  alphabet is uppercase or lowercase.
+ * @c: character to be checked.
+ * Return: 1 if the character is uppercase.
+ * 0 if the character is lowercase.
+ */
+int _isupper(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    { 
+        if (c >= 'A' && c <= 'Z')
+        {
+            return (1);
+        }
+	    else
+		return (0);
+    }
+		
+}
---
+++ 0x04-more_functions_nested_loops/f-test.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
+#include "main.h"
+/**
+ * _isupper - tests whether a character in the  alphabet is uppercase or lowercase.
+ * @c: character to be checked.
+ * Return: 1 if the character is uppercase.
+ * 0 if the character is lowercase.
+ */
+int _isupper(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    { 
+        if (c >= 'A' && c <= 'Z')
+        {
+            return (1);
+        }
+	    else
+		return (0);
+    }
+		
+}
