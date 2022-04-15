cc08dfc11ebcdb6cf96d50bae085b57a1e0aa20c
Sylvester254 <slynganga59@gmail.com>
2022-04-15 03:36:39 +0300

    print numbers

--- 0x04-more_functions_nested_loops/f-test.c
+++ 0x04-more_functions_nested_loops/f-test.c
@@ -7,44 +7,22 @@
  */
 int main(void)
 {
-    //print_triangle(2);
-    print_triangle(10);
-    //print_triangle(1);
-    //print_triangle(0);
+    print_numbers();
     return (0);
 }
 #include "main.h"
 /**
- * print_triangle - function that prints a triangle.
- * _putchar - function to print.
- * @size: the size of the triangle
+ * 
+ * 
  */
-void print_triangle(int size)
+void print_numbers(void)
 {
 int i;
-int j;
-if (size > 0)
+for (i = 0; i <= 9; i++)
 {
-for (i = 1; i <= size; i++)
-{
-for (j = size; j >= 1; j--)
-{
-    if (j > i)
-    {
-        _putchar (' ');
-    }
-    else
-    {
-         _putchar ('#');
-    }
+    _putchar (i + '0');
 }
 _putchar ('\n');
-}
-}
-else
-{
-_putchar ('\n');
-}
 }
 #include <unistd.h>
 /**
