e4acd8d0833df75476e54e7a5328c3356c7332f4
Sylvester254 <slynganga59@gmail.com>
2022-04-29 12:20:40 +0300

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,54 +6,27 @@
  */
 int is_prime_number(int n)
 {
-    return (_prime(n, 2));
+return (_prime(n, 2));
 }
-
+/**
+ * _prime - determine if prime
+ *
+ * @n: no to check if prime
+ * @i: counter number
+ * Return: 1
+ */
 int _prime(int n, int i)
 {
 int m;
-    m = n / 2;
-    
-        if ((n % i == 0 && i <= m) || n < 0 || n == 1) 
-        {
-            return (0);
-        }
-        else if (n % i != 0 && i <= m)
-        {
-            return (_prime(n, i + 1));
-        }
-
-        else
-            return (1);
-}
-
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+m = n / 2;
+if ((n % i == 0 && i <= m) || n < 0 || n == 1)
 {
-    int r;
-
-    r = is_prime_number(1);
-    printf("%d\n", r);
-    r = is_prime_number(1024);
-    printf("%d\n", r);
-    r = is_prime_number(16);
-    printf("%d\n", r);
-    r = is_prime_number(17);
-    printf("%d\n", r);
-    r = is_prime_number(25);
-    printf("%d\n", r);
-    r = is_prime_number(-1);
-    printf("%d\n", r);
-    r = is_prime_number(113);
-    printf("%d\n", r);
-    r = is_prime_number(7919);
-    printf("%d\n", r);
-    return (0);
+return (0);
+}
+else if (n % i != 0 && i <= m)
+{
+return (_prime(n, i + 1));
+}
+else
+return (1);
 }
