d9cbba53cedd10298c24372fe6db3b5327f2fdd2
Sylvester254 <slynganga59@gmail.com>
2022-05-03 21:27:17 +0300

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,13 +9,14 @@
 int main(int argc, char *argv[])
 {
 int p;
-if(argc == 3)
+if (argc == 3)
 {
 p = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", p);
 return (0);
 }
-else{
+else
+{
 printf("Error\n");
 return (1);
 }
