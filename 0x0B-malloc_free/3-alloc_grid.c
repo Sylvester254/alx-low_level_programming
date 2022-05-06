5ff064b61cdd0a7f209e74ad010c68e85234de4f
Sylvester254 <slynganga59@gmail.com>
2022-05-07 02:40:10 +0300

    2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,11 +1,38 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * @brief 
- * 
- * @param width 
- * @param height 
- * @return int** 
+ * alloc_grid - creates 2D int array
+ *
+ * @width: width size
+ * @height: height size
+ * Return: int**
  */
 int **alloc_grid(int width, int height)
-{}
+{
+int i, j;
+	int **s;
+
+	i = j = 0;
+	if (height < 1)
+		return (NULL);
+	s = (int **)malloc(height * sizeof(s));
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		s[i] = malloc(width * sizeof(int));
+		if (s[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(s[j]);
+			free(s);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			s[i][j] = 0;
+	}
+	return (s);
+}
