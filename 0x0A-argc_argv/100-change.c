04153c685018b333cdf3045a2d620e483a6a718f
Sylvester254 <slynganga59@gmail.com>
2022-05-09 21:20:44 +0300

    minimal no. of coins for change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-    int cents, coins = 0;
+int cents, coins = 0;
 if (argc == 1 || argc > 2)
 {
 printf("Error\n");
@@ -17,10 +17,10 @@ return (1);
 cents = atoi(argv[1]);
 while (cents > 0)
 {
-if(cents >= 25)
+if (cents >= 25)
 {
 cents -= 25;
-}    
+}
 else if (cents >= 10)
 {
 cents -= 10;
