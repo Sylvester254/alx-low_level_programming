b843fd9beb1ae5446624f9a5266d4c4f1cca484e
Sylvester254 <slynganga59@gmail.com>
2022-05-11 00:53:10 +0300

    array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,20 +9,18 @@
  */
 int *array_range(int min, int max)
 {
-int i, size;
+int *array, i, size;
 if (min > max)
 {
 return (NULL);
 }
 size = max - min + 1;
-int array = malloc(sizeof(int) * size);
+array = malloc(sizeof(int) * size);
 if (array == NULL)
 {
 return (NULL);
 }
 for (i = 0; i < size; i++)
-{
 array[i] = min++;
-}
 return (array);
 }
