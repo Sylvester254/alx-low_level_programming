2e97ddc50f3b2f894e7294b5b4e583bb8b326921
Sylvester254 <slynganga59@gmail.com>
2022-05-11 00:59:15 +0300

    concat update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,34 +11,32 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *s;
-unsigned int i1, i2, k, size;
-if (s1 == NULL)
-{
-s1 = "";
-}
-if (s2 == NULL)
-{
-s2 = "";
-}
-for (i1 = 0; s1[i1]; i1++)
-;
-for (i2 = 0; s2[i2]; i2++)
-;
-if (i2 > n)
-{
-i2 = n;
-}
-size = i1 + i2 + 1;
-s = malloc(sizeof(char) * size);
-if (s == NULL)
-{
-return (NULL);
-}
-for (k = 0; k < size - 1; k++)
-{
-k < i1 ? (s[k] = s1[k]) : (s[k] = s2[k - i1]);
-}
-s[size] = '\0';
-return (s);
+	char *s;
+	unsigned int len = n, index;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	s = malloc(sizeof(char) * (len + 1));
+
+	if (s == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		s[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		s[len++] = s2[index];
+
+	s[len] = '\0';
+
+	return (s);
 }
