fc9c8d29f74690a4f3f18ceff8a47e8f217b8ae8
Sylvester254 <slynganga59@gmail.com>
2022-05-11 00:35:15 +0300

    allocate memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,9 +13,9 @@ int *a;
 unsigned int i;
 if (nmemb == 0 || size == 0)
 {
-return (0);
+return (NULL);
 }
-a = malloc(nmemb * size);
+a = malloc(size * nmemb);
 if (a == NULL)
 {
 return (NULL);
@@ -25,4 +25,55 @@ for (i = 0; i < nmemb; i++)
 a[i] = 0;
 }
 return (a);
+}
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *a;
+
+    a = _calloc(98, sizeof(char));
+    strcpy(a, "Best");
+    strcpy(a + 4, " School! :)\n");
+    a[97] = '!';
+    simple_print_buffer(a, 98);
+    free(a);
+    return (0);
 }
