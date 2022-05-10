e10227702b89853f2efeb2ef857a987e70eb13c2
Sylvester254 <slynganga59@gmail.com>
2022-05-10 23:48:18 +0300

    concatenate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,6 +10,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+char *s;
 int i1, i2, k, size;
 if (s1 == NULL)
 {
@@ -19,22 +20,21 @@ if (s2 == NULL)
 {
 s2 = "";
 }
-for (i1 = 0; i1 < s1[i1]; i1++)
+for (i1 = 0; s1[i1]; i1++)
 ;
-for (i2 = 0; i2 < s2[i2]; i2++)
+for (i2 = 0; s2[i2]; i2++)
 ;
 if (i2 > n)
 {
-i2 == n;
+i2 = n;
 }
-
 size = i1 + i2 + 1;
-char s = malloc(sizeof(char) * size);
+s = malloc(sizeof(char) * size);
 if (s == NULL)
 {
 return (NULL);
 }
-for (k = 0; k < size - 1; k++)
+for (k = 0; k < size; k++)
 {
 k < i1 ? (s[k] = s1[k]) : (s[k] = s2[k - i1]);
 }
