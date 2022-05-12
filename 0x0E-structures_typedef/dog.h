3c774a2186f292015ef8b18a6ab413e6b43bbcd7
Sylvester254 <slynganga59@gmail.com>
2022-05-13 00:21:45 +0300

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - combines name age and owner of a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner:owner's name
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
