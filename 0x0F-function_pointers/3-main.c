94f2243ad87c80f1a6e2e2eef0250e2b21cfbd37
Sylvester254 <slynganga59@gmail.com>
2022-05-17 01:43:17 +0300

    program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,6 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "3-calc.h"
 /**
  * main - check functions
  *
@@ -20,7 +20,7 @@ if (get_op_func(argv[2]) == NULL)
 {
 printf("Error\n"), exit(99);
 }
-if (num2 == 0 && (argv[2] == '%' || argv[2] == '/'))
+if (num2 == 0 && (*argv[2] == '%' || *argv[2] == '/'))
 {
 printf("Error\n"), exit(100);
 }
