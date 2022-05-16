105f29a312282b887c51165eb31a47d0c3511be8
Sylvester254 <slynganga59@gmail.com>
2022-05-16 22:27:53 +0300

    prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: string name
+ * @f: function pointer to char pointer
+ * Return: null
+ */
+void print_name(char *name, void (*f)(char *))
+{
+(*f)(name);
+}
