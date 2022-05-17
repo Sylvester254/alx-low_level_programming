6ee56f08ed8072243f244874bac52a7a9967da04
Sylvester254 <slynganga59@gmail.com>
2022-05-18 00:20:24 +0300

    print anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -74,16 +74,3 @@ i++;
 }
 printf("\n");
 }
-
-#include "variadic_functions.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_all("ceis", 'B', 3, "stSchool");
-    return (0);
-}
