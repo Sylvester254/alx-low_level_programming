b7b7a34fd7268c99232dfd33498c9e5ac988e262
Sylvester254 <slynganga59@gmail.com>
2022-05-18 00:30:56 +0300

    print anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,14 +1,11 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
+#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void p_int(va_list a);
-void p_float(va_list a);
-void p_char(va_list a);
-void p_string(va_list a);
 /**
  * struct printAll - structure definition
  * @type: type
