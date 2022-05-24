05d759171ecb2f201c19275db7bc2ed790f41487
Sylvester254 <slynganga59@gmail.com>
2022-05-24 21:51:29 +0300

    list length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * list_len - gives the number of elements in a linked list_t list.
+ * list_len - gives the number of elements in a linked list_t.
  *
  * @h: singly linked list
  * Return: size_t 
