6133810722168ab0b2f4a97ca6f17c4a341aee61
Sylvester254 <slynganga59@gmail.com>
2022-05-24 21:59:20 +0300

    list length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,14 +4,13 @@
  * list_len - gives the number of elements in a linked list_t.
  *
  * @h: singly linked list
- * Return: size_t 
+ * Return: size_t
  */
 size_t list_len(const list_t *h)
 {
 size_t el;
 while (h)
 {
-printf("[%d] %s\n", h->len, h->str);
 el++;
 h = h->next;
 }
