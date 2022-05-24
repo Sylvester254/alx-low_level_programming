9203c5335bc397cab57fe79520fe7fd28ef25c1a
Sylvester254 <slynganga59@gmail.com>
2022-05-24 23:31:27 +0300

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * bfr_main - function to execute before main
+ *
+ */
+void __attribute__((constructor)) bfr_main()
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
