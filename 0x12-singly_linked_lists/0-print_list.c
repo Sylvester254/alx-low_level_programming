956106032788523839bc0be73c6e16a704cde926
Sylvester254 <slynganga59@gmail.com>
2022-05-24 21:31:29 +0300

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,10 +9,19 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t i;
-    for (i = 0; h; i++)
-    {
-        printf("[%d] %s\n", h->len, h->str);
-    }
-    return (i);
+size_t nodes = 0;
+while (h)
+{
+if (h->str == NULL)
+{
+printf("[0](nil)\n");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+nodes++;
+h = h->next;
+}
+return (nodes);
 }
