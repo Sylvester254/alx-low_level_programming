326b12c5f4b7c4ccfdb7d2ce1feb80811add1245
Sylvester254 <slynganga59@gmail.com>
2022-05-28 02:04:02 +0300

    sum all elements

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * sum_listint - sum all elements of a list
+ * 
+ * @head:first node
+ * Return: sum of all elements
+ */
+int sum_listint(listint_t *head)
+{
+int sum;
+for (sum = 0; head; sum += head->n, head = head->next)
+    ;
+return (sum);
+}
