a69e512a8c039c17e1735f2b886ffff584497a58
Sylvester254 <slynganga59@gmail.com>
2022-05-28 00:59:13 +0300

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_listint - print sll elements of list_t
+ *
+ * @h: singly linked list to print
+ * Return: size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t i;
+for (i = 0;h; i++)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
+return (i);
+}
