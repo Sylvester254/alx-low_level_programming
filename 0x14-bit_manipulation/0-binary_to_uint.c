089b26b970fb161c3fa8224cf34e577e368d89b9
Sylvester254 <slynganga59@gmail.com>
2022-05-31 23:38:38 +0300

    binary to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - function that returns length of a string.
+ * @s: a string
+ * Return: integer value.
+ */
+int _strlen(char *s)
+{
+int i;
+
+i = strlen(s);
+return (i);
+}
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ *
+ * @b: pointer to a string of 0 and 1 chars
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int j = 0;
+int i, leng;
+if (b == NULL)
+{
+return (0);
+}
+leng = _strlen(b);
+for (i = 0; i != leng; i++)
+{
+if (b[leng - i - 1] == '0')
+{
+j += 1 << i;
+}
+else if (b[leng - i - 1] != '0')
+{
+return(0);
+}
+}
+return (j);
+}
