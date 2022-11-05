bf2fe9b9344d5264427161e97f072180b17e3c56
Sylvester254 <slynganga59@gmail.com>
2022-11-05 15:38:47 +0300

    interpolation fix

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -16,19 +16,26 @@ int interpolation_search(int *array, size_t size, int value)
         int high = size - 1;
         size_t pos = 0;
 
-        while ((array[high] != array[low]) && (value >= array[low]) && (value <= array[high]))
+        while ((array[high] != array[low]))
         {
             pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
-
+            if (pos >= size)
+            {
+                printf("Value checked array[%d] is out of range\n", (int)pos);
+                break;
+            }
+            printf("Value checked array[%d] = [%d]\n", (int)pos, *(array + pos));
             if (array[pos] < value)
                 low = pos + 1;
             else if (value < array[pos])
                 high = pos - 1;
             else
-                printf("Value checked array[%d] = [%d]\n", (int)pos, *(array + pos));
-            return pos;
+                return pos;
         }
-
+        /** This ternary function is same as
+         * the expression from line 39,
+         * return (value == array[low] ? (int)low : -1);
+         */
         if (value == array[low])
         {
             printf("Value checked array[%d] = [%d]\n", (int)low, *(array + low));
